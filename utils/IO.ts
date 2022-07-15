987da21fdd22e5a5d1f27d268d0e4840f794b6f8
Popenkov Anton <popenkovaa@yandex.ru>
2022-07-15 20:58:50 +0300

    .

--- utils/IO.ts
+++
@@ -1,7 +0,0 @@
-const options = {
-  root: document.querySelector('[data-scroll-root]'),
-  rootMargin: '0px',
-  threshold: 1.0,
-};
-
-export {};
--- utils/scroller.ts
+++
@@ -1,3 +0,0 @@
-export const scroller = () => {
-  console.log('scroll');
-};
